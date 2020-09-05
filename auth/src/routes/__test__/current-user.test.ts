17f778f1644956d0a86084048308706794065f3c
yomi <adepitanoriyomi11@yahoo.com>
2020-09-05 17:16:58 +0100

    updated test current user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -18,7 +18,7 @@ it("responds with details about current user", async () => {
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
-    //.send()
+    .send()
     .expect(200);
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
