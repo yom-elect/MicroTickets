85d19da351686a9ad72c9376393d595caee496d0
yomi <adepitanoriyomi11@yahoo.com>
2020-09-05 16:42:46 +0100

    dev branch test

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,20 +1,20 @@
 import axios from "axios";
+//this is to test event work flow on githhub
 
 const buildClient = ({ req }) => {
-  if (typeof window === "undefined") {
-    // server request http://SERVICENAME.NAMESPACE.svc.cluster.local
+    if (typeof window === "undefined") {
+        // server request http://SERVICENAME.NAMESPACE.svc.cluster.local
 
-    return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-      headers: req.headers,
-    });
-  } else {
-    // browser request
-    return axios.create({
-      baseURL: "/",
-    });
-  }
+        return axios.create({
+            baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+            headers: req.headers,
+        });
+    } else {
+        // browser request
+        return axios.create({
+            baseURL: "/",
+        });
+    }
 };
 
 export default buildClient;
